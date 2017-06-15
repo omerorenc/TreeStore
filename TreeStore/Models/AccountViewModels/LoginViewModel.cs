96fe26fcf66f1964a8d14963952919a26f68aa04
ArzuGedik <arzugedik312@gmail.com>
2017-06-15 04:15:11 +0300

    Login bug fixed

--- TreeStore/Models/AccountViewModels/LoginViewModel.cs
+++ TreeStore/Models/AccountViewModels/LoginViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TreeStore.Models.AccountViewModels
 {
-    public class LoginViewModel:BaseEntity
+    public class LoginViewModel : BaseEntity
     {
         [Required]
         [EmailAddress]
@@ -16,10 +16,28 @@ namespace TreeStore.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre(Tekrar)")]
+        [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor. Lütfen tekrar deneyin.")]
+        public string ConfirmPassword { get; set; }
+
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
+
+        [Required]
+        [Display(Name = "Firma Adı")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Firma Adresi")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Lütfen firma telefonunu giriniz.")]
+        [Display(Name = "Firma Telefonu")]
         public string Phone { get; set; }
-        public string Logo { get; set; }
+
+        [Display(Name = "Firma Fax")]
+        public string Fax { get; set; }
     }
-}
+        }
+       
+
